f3e2deacb5438a81b87240bcb9eee1f599b21be8
LuisSas <luis@makingdevs.com>
2019-03-18 10:14:28 -0600

    Registering new component

---
+++ index.android.js
@@ -0,0 +1,13 @@
+// Import a library to hel create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a component
+const App = () => {
+  return (
+    <Text> Some Text </Text>
+  );
+}
+
+// Render it to the device
+ReactNative.AppRegistry.registerComponent('albums', () => App);
